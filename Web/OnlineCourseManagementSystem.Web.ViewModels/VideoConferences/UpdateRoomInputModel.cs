a70fcb057bec398137478f4793967c0dd088328c
MiroslavUzunov <miroslav.uzunov23@gmail.com>
2022-04-25 00:34:48 +0300

    Video Conference updated.

---
+++ Web/OnlineCourseManagementSystem.Web.ViewModels/VideoConferences/UpdateRoomInputModel.cs
@@ -0,0 +1,20 @@
+﻿namespace OnlineCourseManagementSystem.Web.ViewModels.VideoConferences
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    using OnlineCourseManagementSystem.Data.Models;
+    using OnlineCourseManagementSystem.Services.Mapping;
+
+    public class UpdateRoomInputModel : IMapFrom<Room>
+    {
+        public string Id { get; set; }
+
+        public bool IsDisplayCameraAllowed { get; set; }
+
+        public bool IsUnmuteMicAllowed { get; set; }
+
+        public bool IsShareScreenAllowed { get; set; }
+    }
+}
